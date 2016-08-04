8efcc831cc5106243baccb2dbca916431370b6b3
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-04 15:52:00 +1200

    everything is working

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var hbs = require('hbs');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -64,6 +67,9 @@ app.use(function(err, req, res, next) {
   //   catDB.cats.push(newCat);
   //   res.redirect('/cats')
   // })
+
+
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var addName
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Enspiral Database of Animals', name: "test" });
 });
 
 router.get('/welcome', function(req, res, next) {
-  res.render('welcome', { title: 'Enspiral Database of Animals', name: 'test' });
+  addName = req.body.name
+  console.log("this is the addName second attempt: ", addName)
+  res.render('welcome', { title: 'Enspiral Database of Animals', name: addName});
 });
 
 router.get('/feed', function(req, res, next) {
   res.render('feed', { name: 'test' });
+});
+
+router.post('/welcome', function(req, res) {
+  addName = req.body.name
+  console.log("this is the addName: ", addName)
+  res.render('./welcome',{ name: addName, title: 'Enspiral Database of Animals' })
+
 });
 
 module.exports = router;
