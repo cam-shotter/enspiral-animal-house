05de37d47db9714c1594c487ed62831485b14971
Max Robinson & Cameron Shotter <pair+Maxwell-Robinson.cam-shotter@devbootcamp.com>
2016-08-04 14:35:23 +1200

    modified routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: "test" });
+  res.render('index', { title: 'Enspiral Database of Animals', name: "test" });
 });
 
 router.get('/welcome', function(req, res, next) {
